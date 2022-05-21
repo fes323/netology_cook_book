487fa197fbe6ed197a7ec2d1a6710b9e2f0bbb17
Maksim Legeyda <maksim.legeyda@yandex.ru>
2022-05-21 21:45:27 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+
+cook_book = {}
+with open("cook_book.txt", encoding='utf-8') as file:
+    for line in file:
+        dish_name = line.strip()
+        counter = file.readline()
+        cook_book[dish_name] = []
+        for i in range(int(counter)):
+            ingredient, quantity, unit = file.readline().split(' | ')
+            cook_book[dish_name].append({
+                'ingredient_name': ingredient,
+                'quantity': int(quantity),
+                'measure': unit.strip()
+            })
+        file.readline()
+        
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            ingredient_name = ingredient['ingredient_name']
+            if ingredient_name not in shop_list:
+                shop_list[ingredient_name] = {'quantity': int(ingredient['quantity']) * person_count, 'measure': ingredient['measure']}
+            else:
+                shop_list[ingredient_name]['quantity'] += int(ingredient['quantity']) * person_count
+    return shop_list
+        
+pprint(cook_book)
+print()
+pprint(get_shop_list_by_dishes(['Утка по-пекински', 'Фахитос', 'Запеченный картофель'], 323))
